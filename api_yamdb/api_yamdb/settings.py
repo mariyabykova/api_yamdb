1e1b883abc0a59493541441546020598cf306d29
Maria Bykova <mariab@bk.ru>
2022-12-12 22:24:20 +0300

    Созданы приложения reviews и фÐapi

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,6 +23,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'reviews.apps.ReviewsConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
