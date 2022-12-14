0a643e983e73bb195bdb108b3c391230538d0d71
Vladislav <vl241095@gmail.com>
2022-12-14 14:47:01 +0500

    Добавлен сериализатор на отзыв ReviewSerializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-
-from reviews.models import Comment, Review
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import get_object_or_404
+from reviews.models import Comment, Review, Title
 
 
 class SignUpSerializer(serializers.Serializer):
@@ -9,6 +10,26 @@ class SignUpSerializer(serializers.Serializer):
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        default=serializers.CurrentUserDefault(),
+        slug_field='username',
+        read_only=True
+    )
+    title = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True
+    )
+
+    def validate(self, data):
+        request = self.context['request']
+        author = request.user
+        title_id = self.context['view'].kwargs.get['title_id']
+        title = get_object_or_404(Title, pk=title_id)
+        if request.method == 'POST':
+            if Review.objects.filter(title=title, author=author):
+                raise ValidationError('Вы уже оставили отзыв '
+                                      'на данное произведение')
+        return data
 
     class Meta:
         model = Review
