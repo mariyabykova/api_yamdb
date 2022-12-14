6db1379826b717f4b10ac87902348c076539f951
Михаил Буланкин <mvl.bulankin@gmail.com>
2022-12-14 23:38:50 +0300

    написал урлы, начал вью и сериалайзеры

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from reviews.models import Comment, Review
+from reviews.models import Category, Comment, Genre, Review, Title
 
 
 class SignUpSerializer(serializers.Serializer):
@@ -19,4 +19,34 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+        lookup_field = 'slug'
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    genre = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(),
+        slug_field='slug',
+        many=True
+    )
+    category = serializers.SlugRelatedField(
+        queryset=Category.objects.all(),
+        slug_field='slug'
+    )
+    rating = serializers.IntegerField(required=False)
+
+    class Meta:
+        model = Title
         fields = '__all__'
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,10 +2,19 @@ from django.conf import settings
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from rest_framework import generics, status, viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
-from api.serializers import SignUpSerializer, CommentSerializer, ReviewSerializer
+from api.serializers import (
+    CategorySerializer,
+    CommentSerializer,
+    GenreSerializer,
+    ReviewSerializer,
+    SignUpSerializer,
+    TitleSerializer
+)
+from reviews.models import Category, Comment, Genre, Review, Title
 from users.models import User
 
 
@@ -43,3 +52,25 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    lookup_field = 'slug'
+    search_fields = ('name',)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    search_fields = ('name',)
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = PageNumberPagination
