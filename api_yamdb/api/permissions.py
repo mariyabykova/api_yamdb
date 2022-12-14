27f88d2a002805a5652cfc90619e31bd80c4a20f
Maria Bykova <mariab@bk.ru>
2022-12-14 13:40:40 +0300

    Добавлен permission ШЫIdAdminOmly

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -2,4 +2,9 @@ from rest_framework import permissions
 
 
 class IsAdminOnly(permissions.BasePermission):
-    pass
+    def has_permission(self, request, view):
+        return (
+                request.user.is_authenticated and
+                (request.user.is_superuser or request.user.role == 'admin'
+                 or request.user.is_staff)
+        )
