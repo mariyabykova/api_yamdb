059046d6cb88fae5a89d296666714c76f4e76533
Maria Bykova <mariab@bk.ru>
2022-12-14 17:20:44 +0300

    Исправлен пермишен

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -23,5 +23,6 @@ class IsModeratorOrReadOnly(permissions.BasePermission):
         return (request.method in permissions.SAFE_METHODS or
                 (request.user.is_authenticated and
                  (request.user.is_superuser or request.user.role == 'admin'
-                  or request.user.role == 'moderator' or obj.author == request.user
+                  or request.user.role == 'moderator'
+                  or obj.author == request.user
                   or request.user.is_staff)))
