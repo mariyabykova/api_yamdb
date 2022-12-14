27d000a8bed8aaebce1932562244fb3235b99aaf
Maria Bykova <mariab@bk.ru>
2022-12-14 17:12:27 +0300

    ИсДобавлены permissions. Исправлен SignupSerializer

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -8,3 +8,23 @@ class IsAdminOnly(permissions.BasePermission):
                 (request.user.is_superuser or request.user.role == 'admin'
                  or request.user.is_staff)
         )
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS or
+                (request.user.is_authenticated and
+                 (request.user.is_superuser or
+                     request.user.role == 'admin' or
+                     request.user.is_staff)))
+
+
+class IsModeratorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS or
+                (request.user.is_authenticated and
+                 (request.user.is_superuser or
+                     request.user.role == 'admin' or
+                     request.user.role == 'moderator' or
+                     obj.author == request.user or
+                     request.user.is_staff)))
--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from reviews.models import Comment, Review
+from reviews.models import Comment, Review, User
 
 
 class SignUpSerializer(serializers.Serializer):
@@ -10,6 +10,20 @@ class SignUpSerializer(serializers.Serializer):
         max_length=150
     )
     email = serializers.EmailField(required=True, max_length=254)
+
+    def validate(self, data):
+        if data['username'] == 'me':
+            raise serializers.ValidationError(
+                'Недопустимое имя пользователя!'
+            )
+        if User.objects.filter(username=data['username'], email=data['email']).exists():
+            return data
+        if (User.objects.filter(username=data['username']).exists()
+                or User.objects.filter(email=data['email']).exists()):
+            raise serializers.ValidationError('Пользователь с такими данными уже существует!')
+        return data
+
+
 
 
 class ReviewSerializer(serializers.ModelSerializer):
