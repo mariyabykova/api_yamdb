5fe9970d0875c14d38061bb7326f009a84c8c16f
Maria Bykova <mariab@bk.ru>
2022-12-14 20:38:21 +0300

    Исправлены пÐпермишены

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,8 +5,7 @@ class IsAdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
                 request.user.is_authenticated and
-                (request.user.is_superuser or request.user.role == 'admin'
-                 or request.user.is_staff)
+                (request.user.is_superuser or request.user.role == 'admin')
         )
 
 
@@ -14,8 +13,7 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS or
                 (request.user.is_authenticated and
-                 (request.user.is_superuser or request.user.role == 'admin'
-                  or request.user.is_staff)))
+                 (request.user.is_superuser or request.user.role == 'admin')))
 
 
 class IsModeratorOrReadOnly(permissions.BasePermission):
@@ -24,5 +22,4 @@ class IsModeratorOrReadOnly(permissions.BasePermission):
                 (request.user.is_authenticated and
                  (request.user.is_superuser or request.user.role == 'admin'
                   or request.user.role == 'moderator'
-                  or obj.author == request.user
-                  or request.user.is_staff)))
+                  or obj.author == request.user)))
