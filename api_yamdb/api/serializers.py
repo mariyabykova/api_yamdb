28303da33f02743d51f1a8536858458dc3b9ffbd
Vladislav <vl241095@gmail.com>
2022-12-15 13:12:40 +0500

    Еще одни правки в сериализаторе

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -39,8 +39,8 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    review = serializers.SlugRelatedField(
-        slug_field='text',
+    author = serializers.SlugRelatedField(
+        slug_field='username',
         read_only=True
     )
 
