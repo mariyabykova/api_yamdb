51ab176e09cb649acbb6390225096f235700afc9
Михаил Буланкин <mvl.bulankin@gmail.com>
2022-12-20 15:27:15 +0300

    добавил универсальную команду загрузки файлов

---
+++ api_yamdb/reviews/management/commands/load_universal_data.py
@@ -0,0 +1,25 @@
+import csv
+
+from django.core.management import BaseCommand
+from django.apps import apps
+
+
+class Command(BaseCommand):
+    help = 'Creating model objects according the file path specified'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--path', type=str, help="file path")
+        parser.add_argument('--model_name', type=str, help="model name")
+        parser.add_argument(
+            '--app_name',
+            type=str,
+            help="django app name that the model is connected to"
+        )
+
+    def handle(self, *args, **options):
+        file_path = options['path']
+        model = apps.get_model(options['app_name'], options['model_name'])
+        with open(file_path, 'rt') as csv_file:
+            reader = csv.DictReader(csv_file, delimiter=',')
+            for row in reader:
+                model.objects.create(**row)
